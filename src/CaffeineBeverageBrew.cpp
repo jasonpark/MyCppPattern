f4cd943e0dfac24b43907ad325f9627f37bea3c5
jasonpark <pathstar@hanmail.net>
2013-05-26 11:56:13 +0900

    Modify CaffeineBererageBrew

--- src/CaffeineBeverageBrew.cpp
+++ src/CaffeineBeverageBrew.cpp
@@ -4,3 +4,13 @@ static void brewCoffe()
 {
    std::cout << "dripping Coffee through filter\n";
 }
+
+static void brewTea()
+{
+  std::cout << "steeping Tea\n";
+}
+
+static int amountWaterMl(int ml)
+{
+  return ml;
+}
